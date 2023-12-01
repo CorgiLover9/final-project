96774250516012c765a5305e112a284986461494
CorgiLover9 <144844302+CorgiLover9@users.noreply.github.com>
2023-12-01 09:09:54 -0500

    more colors

--- firework.js
+++ firework.js
@@ -2,7 +2,7 @@
 // http://codingtra.in
 // https://youtu.be/CKeyIbT3vXI
 
-const COLORS = [240, 330, 0, 108, 60];
+const COLORS = [240, 330, 0, 108, 60, 280, 168];
 class Firework {
   
   constructor() {
